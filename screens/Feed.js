4ffe182e63a8ad42ab4c0151012b939776448f44
Heny Patel <heny09112@gmail.com>
2023-10-09 18:30:10 -0400

    added styles

--- screens/Feed.js
+++ screens/Feed.js
@@ -11,6 +11,7 @@ import {
 import { RFValue } from 'react-native-responsive-fontsize';
 import { FlatList } from 'react-native-gesture-handler';
 
+let posts = require("./temp_posts.json");
 
 export default class Feed extends Component {
     constructor(props) {
@@ -52,5 +53,36 @@ export default class Feed extends Component {
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.8,
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: RFValue(28),
+    },
+    cardContainer: {
+        flex: 0.85
+    }
 })
