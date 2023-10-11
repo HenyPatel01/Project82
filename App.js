dec83eb464116b78ddc3ec4f8b7f140e3bdedbfc
Heny Patel <heny09112@gmail.com>
2023-10-10 20:00:46 -0400

    fixed bugs

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from './navigation/DrawerNavigation';
+import { AppRegistry } from 'react-native';
 
 export default function App() {
   return (
