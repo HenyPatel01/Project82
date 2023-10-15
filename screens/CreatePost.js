495eeb87cb138e3d4aef290410fc81d75f5b2770
Heny Patel <heny09112@gmail.com>
2023-10-15 17:18:47 -0400

    loaded images

--- screens/CreatePost.js
+++ screens/CreatePost.js
@@ -15,16 +15,32 @@ import { RFValue } from "react-native-responsive-fontsize";
 import DropDownPicker from "react-native-dropdown-picker";
 
 export default class CreatePost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            previewImage: "image_1",
+            dropdownHeight: 40
+        }
+    }
+    
     render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Text>Create Post</Text>
+        let preview_images = {
+            image_1: require("../assets/image_1.jpg"),
+            image_2: require("../assets/image_2.jpg"),
+            image_3: require("../assets/image_3.jpg"),
+            image_4: require("../assets/image_4.jpg"),
+            image_5: require("../assets/image_5.jpg"),
+            image_6: require("../assets/image_6.jpg"),
+            image_7: require("../assets/image_7.jpg")
+        };
+        return(
+            <View>
+
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    
+})
