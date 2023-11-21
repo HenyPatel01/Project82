65c50cf3a5bb4ab491a220e32a4da4da29c6ce6a
Heny Patel <heny09112@gmail.com>
2023-11-20 19:18:38 -0500

    added addPost function

--- screens/CreatePost.js
+++ screens/CreatePost.js
@@ -31,6 +31,41 @@ export default class CreatePost extends Component {
         this.fetchUser();
     }
 
+    async addPost() {
+        if (
+            this.state.caption
+        ) {
+            let postData = {
+                preview_image: this.state.previewImage,
+                caption: this.state.caption,
+                author: firebase.auth().currentUser.displayName,
+                created_on: new Date(),
+                author_uid: firebase.auth().currentUser.uid,
+                profile_image: this.state.profile_image,
+                likes: 0
+            };
+            await firebase
+                .database()
+                .ref(
+                    "/posts/" + 
+                    Math.random()
+                        .toString(36)
+                        .slice(2)
+                )
+                .set(postData)
+                .then(function (snapshot) { });
+            this.props.setUpdateToTrue();
+            this.props.navigation.navigate("Feed");
+        } else {
+            Alert.alert(
+                "Error",
+                "All fields are required",
+                [{ text: "OK", onPress: () => console.log("OK Pressed")}],
+                { cancelable: false}
+            );
+        }
+    }
+
     fetchUser = () => {
         let theme;
         firebase
@@ -124,7 +159,7 @@ export default class CreatePost extends Component {
                         />
                         <View style={styles.submitButton}>
                             <Button
-                                onPress={() => this.addStory()}
+                                onPress={() => this.addPost()}
                                 title="Submit"
                                 color="red"
                             />
